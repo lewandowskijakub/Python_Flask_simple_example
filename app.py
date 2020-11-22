fc8d472ab3ab007c22b4c89cb48bfb5739eb3f11
lewandowskijakub <lewyjb@gmail.com>
2020-11-22 12:31:12 +0100

    Super Sprinter - added new stories implemented

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 import user_story
 
@@ -19,6 +18,20 @@ def get_story(story_id):
     story = user_story.get_user_story(story_id)
     headers = user_story.get_headers()
     return render_template("story.html", story=story, headers=headers)
+
+
+@app.route('/add-story-form')
+def add_story_form():
+    headers = user_story.get_headers()
+    return render_template("story-form.html", headers=headers)
+
+
+@app.route('/stories', methods=["POST"])
+def add_story():
+    data = dict(request.form)
+    print(data)
+
+    return redirect(url_for("index"))
 
 
 if __name__ == '__main__':
