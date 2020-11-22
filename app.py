152b8870182373b48572ba7aaf718faa5b90bf6f
lewandowskijakub <lewyjb@gmail.com>
2020-11-22 11:09:49 +0100

    "Super sprinter" first commit

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
+import user_story
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-@app.route('/test')
-def test():
-    user = {'name': 'CodeCool222'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['name'] + '''!</h1>
-    </body>
-</html>'''
+def index():
+    user_stories = user_story.get_user_stories()
+    headers = user_story.get_headers()
+    return render_template("index.html", stories=user_stories, headers=headers)
 
 
 if __name__ == '__main__':
---
+++ user_story.py
@@ -0,0 +1,18 @@
+HEADERS = ["Id", "Story Title", "User Story", "Acceptance Criteria", "Business Value", "Estimation", "Status"]
+
+
+def get_headers():
+    return HEADERS
+
+
+def get_user_stories():
+    return [{HEADERS[0]: 1, "Story Title": "Some title", "User Story": "After click button is everything okay",
+             "Acceptance Criteria": "Login button works", "Business Value": 100, "Estimation": 2,
+             "Status": "pending"},
+            {HEADERS[0]: 2, "Story Title": "Some title2", "User Story": "After login is logon",
+             "Acceptance Criteria": "User can login", "Business Value": 92, "Estimation": 1,
+             "Status": "done"},
+            {HEADERS[0]: 3, "Story Title": "Some title3", "User Story": "33333333333333333333333",
+             "Acceptance Criteria": "works", "Business Value": 87, "Estimation": 10,
+             "Status": "in-progress"}
+            ]
